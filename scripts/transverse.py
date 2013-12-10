27dd8654a40303e96680d449543bd2866004c426
Thiago Emmanuel Pereira <thiago.manel@gmail>
2013-12-10 15:20:25 -0300

    metadata collector

--- scripts/transverse.py
+++ scripts/transverse.py
@@ -1,21 +1,30 @@
 import os
 import sys
+import stat
 
 def info(fullpath):
+
     try:
-        size = os.path.getsize(fullpath)
+        _stat = os.stat(fullpath)
     except OSError:
-        size = -1
-    return "#".join([str(os.path.isdir(fullpath)), str(os.path.isfile(fullpath)),
-                    str(os.path.islink(fullpath)), fullpath,
-                    os.path.realpath(fullpath), str(size)])
+        _stat = os.lstat(fullpath)
+
+    mode = _stat.st_mode
+    size = _stat.st_size
+
+    return "#".join([str(stat.S_ISDIR(mode)), str(stat.S_ISCHR(mode)),\
+                     str(stat.S_ISBLK(mode)), str(stat.S_ISREG(mode)),\
+                     str(stat.S_ISFIFO(mode)), str(stat.S_ISSOCK(mode)),\
+                     str(stat.S_ISLNK(mode)),\
+                     fullpath, os.path.realpath(fullpath),\
+                     str(size)])
 
 if __name__ == "__main__":
     """
         It transverses the file system collecting file types and sizes.
         python $0 rootname > out
     """
-    for dirpath, dirnames, filenames in os.walk(sys.argv[1]):
+    for dirpath, dirnames, filenames in os.walk(sys.argv[1], followlinks=True):
         for fname in filenames:
             print info(os.path.join(dirpath, fname))
         for dname in dirnames:
